57bbc359348a5b7d9ce01966098d18a62e79ffb5
MartinLedel <martinledel@gmail.com>
2019-12-12 17:19:43 +0100

    Done with kmom06 testing.

--- test/Book/BookControllerTest.php
+++ test/Book/BookControllerTest.php
@@ -3,21 +3,18 @@ namespace Anax\Book;
 
 use Anax\DI\DIFactoryConfig;
 use PHPUnit\Framework\TestCase;
+use Anax\Database\Database;
 
 /**
  * Test the SampleController.
  */
 class BookControllerTest extends TestCase
 {
-    private $sqliteOptions = [
-        "dsn" => "sqlite:memory::",
-        "verbose" => false
-    ];
-    private $rows = [
-        [1, "Metro: Exodus", "Dmitry Glukhovsky", "MetroExodus.jpg"],
-        [2, "LOTR", "J.R.R Tolkien", "lotr.jpg"],
-        [3, "Leviathan Wakes", "James S. A. Corey", "lw.jpg"],
-    ];
+    // private $rows = [
+    //     [1, "Metro: Exodus", "Dmitry Glukhovsky", "MetroExodus.jpg"],
+    //     [2, "LOTR", "J.R.R Tolkien", "lotr.jpg"],
+    //     [3, "Leviathan Wakes", "James S. A. Corey", "lw.jpg"],
+    // ];
     private $db;
     protected $controllerTest;
 
@@ -30,6 +27,10 @@ class BookControllerTest extends TestCase
         // Setup di
         $di = new DIFactoryConfig();
         $di->loadServices(ANAX_INSTALL_PATH . "/config/di");
+        $di->get("configuration")->setBaseDirectories([
+            ANAX_INSTALL_PATH . "/test/config",
+            ANAX_INSTALL_PATH . "/config",
+        ]);
 
         $this->di = $di;
         // Setup the controller
@@ -37,9 +38,18 @@ class BookControllerTest extends TestCase
         $this->controllerTest->setDI($this->di);
 
         // Setup the db
-        $this->db = new Database();
-        $this->db->setOptions($this->sqliteOptions);
-        $this->db->connect();
+        $this->db = $this->di->get("dbqb")->connect();
+
+        // Create a temp table
+        $sql = <<<EOD
+CREATE TABLE Book (
+    "id" INTEGER PRIMARY KEY NOT NULL,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "image" TEXT NOT NULL
+);
+EOD;
+        $this->db->execute($sql);
     }
 
     /**
@@ -57,7 +67,6 @@ class BookControllerTest extends TestCase
     {
         // Test the controller action
         $res = $this->controllerTest->indexActionGet();
-        var_dump($res);
         $this->assertInstanceOf("Anax\Response\Response", $res);
         $this->assertInstanceOf("Anax\Response\ResponseUtility", $res);
     }
@@ -65,36 +74,82 @@ class BookControllerTest extends TestCase
     /**
      * Test the route "info".
      */
-    // public function testCreateActionGet()
-    // {
-    //     $request = $di->get("request");
-    //     // Test the controller action
-    //     $request->setGet("ip", "1.160.10.240");
-    //     $request->setGet("kmom", "01");
-    //     $res = $controllerTest->createActionGet();
-    //     $this->assertInstanceOf("Anax\Response\Response", $res);
-    //     $this->assertInstanceOf("Anax\Response\ResponseUtility", $res);
-    // }
-    //
-    // public function testDeleteActionGet()
-    // {
-    //     $request = $di->get("request");
-    //     // Test the controller action
-    //     $request->setGet("ip", "1.160.10.240");
-    //     $request->setGet("kmom", "02");
-    //     $res = $controllerTest->DeleteActionGet();
-    //     $this->assertInstanceOf("Anax\Response\Response", $res);
-    //     $this->assertInstanceOf("Anax\Response\ResponseUtility", $res);
-    // }
-    //
-    // public function testUpdateActionGet()
-    // {
-    //     $request = $di->get("request");
-    //     // Test the controller action
-    //     $request->setGet("ip", "1.160.10.240");
-    //     $request->setGet("kmom", "02");
-    //     $res = $controllerTest->updateActionGet();
-    //     $this->assertInstanceOf("Anax\Response\Response", $res);
-    //     $this->assertInstanceOf("Anax\Response\ResponseUtility", $res);
-    // }
+    public function testCreateAction()
+    {
+        $request = $this->di->get("request");
+        $request->setServer("REQUEST_METHOD", "POST");
+        // Test the controller action
+        $request->setPost("anax/htmlform-id", "anax/htmlform");
+        $request->setPost("Title", "Metro: Exodus");
+        $request->setPost("Author", "Dmitry Glukhovsky");
+        $request->setPost("Image", "MetroExodus.jpg");
+        $request->setPost("submit", "Create book");
+        $res = $this->controllerTest->createAction();
+        $this->assertInstanceOf("Anax\Response\Response", $res);
+        $this->assertInstanceOf("Anax\Response\ResponseUtility", $res);
+
+        $request->setPost("anax/htmlform-id", "anax/htmlform");
+        $request->setPost("Title", "The Lord of the Rings");
+        $request->setPost("Author", "J. R. R. Tolkien");
+        $request->setPost("Image", "lotr.jpg");
+        $request->setPost("submit", "Create book");
+        $res = $this->controllerTest->createAction();
+        $this->assertInstanceOf("Anax\Response\Response", $res);
+        $this->assertInstanceOf("Anax\Response\ResponseUtility", $res);
+
+        // Test db
+        $sql = "SELECT * FROM Book WHERE id = 1;";
+        $res = $this->db->execute($sql);
+        $res = $this->db->fetch();
+        $this->assertEquals(1, $res->id);
+        $this->assertEquals("Metro: Exodus", $res->title);
+
+        $sql = "SELECT * FROM Book WHERE id = 2;";
+        $res = $this->db->execute($sql);
+        $res = $this->db->fetch();
+        $this->assertEquals(2, $res->id);
+        $this->assertEquals("The Lord of the Rings", $res->title);
+    }
+
+    public function testDeleteAction()
+    {
+        $request = $this->di->get("request");
+        $request->setServer("REQUEST_METHOD", "POST");
+        // Test the controller action
+        $request->setPost("anax/htmlform-id", "Anax\Book\HTMLForm\DeleteForm");
+        $request->setPost("select", "2");
+        $request->setPost("submit", "Delete book");
+        $res = $this->controllerTest->DeleteAction();
+        $this->assertInstanceOf("Anax\Response\Response", $res);
+        $this->assertInstanceOf("Anax\Response\ResponseUtility", $res);
+
+        // Test db
+        $sql = "SELECT * FROM Book WHERE id = 2;";
+        $res = $this->db->execute($sql);
+        $res = $this->db->fetch();
+        $this->assertEquals(null, $res);
+    }
+
+    public function testUpdateAction()
+    {
+        $request = $this->di->get("request");
+        $request->setServer("REQUEST_METHOD", "POST");
+        // Test the controller action
+        $request->setPost("anax/htmlform-id", "Anax\Book\HTMLForm\UpdateForm");
+        $request->setPost("Id", "1");
+        $request->setPost("Title", "Metro: Exodus - Redemption");
+        $request->setPost("Author", "Dmitry Glukhovsky");
+        $request->setPost("Image", "MetroExodus.jpg");
+        $request->setPost("submit", "Update book");
+        $res = $this->controllerTest->updateAction("1");
+        $this->assertInstanceOf("Anax\Response\Response", $res);
+        $this->assertInstanceOf("Anax\Response\ResponseUtility", $res);
+
+        // Test db
+        $sql = "SELECT * FROM Book WHERE id = 1;";
+        $res = $this->db->execute($sql);
+        $res = $this->db->fetch();
+        $this->assertEquals(1, $res->id);
+        $this->assertEquals("Metro: Exodus - Redemption", $res->title);
+    }
 }
